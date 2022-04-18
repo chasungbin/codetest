ad3a9d1eb7dbb1be1ee9e956f8f41111f39189ad
chasungbin <97427142+chasungbin@users.noreply.github.com>
2022-04-18 09:08:52 +0900

    if.1

--- if/2480/app.js
+++ if/2480/app.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const stdin = fs.readFileSync('./input.txt').toString().split('\n');
 let a = Number( stdin[0])
 let b = Number( stdin[1])
+let c = Number( stdin[2])
 if( a === b === c){
     console.log(10000+a*1000)
  }else if(a!==b||a!==c||c!==b){
