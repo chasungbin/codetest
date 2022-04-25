dbb3a356334beea660cfb68bc0f8c70f2229cefe
chasungbin <97427142+chasungbin@users.noreply.github.com>
2022-04-25 11:05:42 +0900

    ss

--- array/최대_최소/app.js
+++ array/최대_최소/app.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const input = fs.readFileSync('./input.txt').toString().split('\n');
-let num  = input[1]
-console.log(Number(num).sort((a,b)=>{
-    a - b
-}))
+let count = Number(input[0]);
+let num  = input[1].split(' ').map((a)=>+a)
+num.sort((a,b)=>
+    a-b
+)
+console.log(num[0],num[num.length-1])
