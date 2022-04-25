7671494714f4c09cfe0a64b63ecf50cc5a63d6d3
chasungbin <97427142+chasungbin@users.noreply.github.com>
2022-04-25 09:59:07 +0900

    forok

--- for/더하기_사이클/app.js
+++ for/더하기_사이클/app.js
@@ -1,16 +1,20 @@
 const fs = require('fs');
 const input = fs.readFileSync('./input.txt').toString();
-let origin = Number(input);
+let addN = 0
 let count = 0;
-console.log(origin)
+let N = +input;
+let isNotSame = true; 
 
-while(true){
-    let sum =Math.floor(input/10) + (input%10);
-    console.log('sum',sum)
-    let input = (input%10)*10+(sum%10)
-    console.log('input',input)
-    count++;
-    if(origin === input) break;
+//Math.floor() : 소수점 이하를 버림한다.
+//Math.ceil() : 소수점 이하를 올림한다.
+//Math.round() : 소수점 이하를 반올림한다.
+while(isNotSame) {
+    addN = Math.floor(N/10) + N%10; 
+    console.log('시험',addN%10)
+    N = N%10*10 + addN%10;
+    count++
+    if(N==input){
+        console.log(count);
+        isNotSame = false;
+    }
 }
-console.log(count);
-  
