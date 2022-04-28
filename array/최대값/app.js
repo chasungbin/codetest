ac84a2e8a4262f900ca9a959a81d32e571cbdaf2
chasungbin <97427142+chasungbin@users.noreply.github.com>
2022-04-28 20:47:55 +0900

    fe

--- array/최대값/app.js
+++ array/최대값/app.js
@@ -16,3 +16,14 @@ for(let i=1; i<9; i++){
 console.log(max);
 //0번째 부터 시작하기에 +1
 console.log(count+1);
+
+//다른 사람들이 푼 풀이 
+// Math메소드 사용 그중 max
+//Math.max()함수는 입력값으로 받은 0개 이상의 숫자 중 가장 큰 숫자를 반환합니다.
+/* apply(thisArg, [argsArray])
+this 인자를 첫번째 인자로 받고, 두번째 인자로는 배열을 받음 */
+let maxs = Math.max.apply(null, num)
+//findIndex() 메서드는 주어진 판별 함수를 만족하는 배열의 첫 번째 요소에 대한 인덱스를 반환합니다. 만족하는 요소가 없으면 -1을 반환합니다.
+let order = (num.findIndex(num => num === max)) + 1;
+
+console.log(maxs + '\n' + order);
